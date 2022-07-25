c88e1f7e62d06f322fa6a681f618590eb9691ef3
Tanvi Shah <tanvishah1409@gmail.com>
2022-07-25 17:13:42 +0530

    Update the confidential changes

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'polysili_polysilirrigation_web' );
+define( 'DB_NAME', '' );
 
 /** MySQL database username */
-define( 'DB_USER', 'polysili_website' );
+define( 'DB_USER', '' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'WQFq]rAtQG^k' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
